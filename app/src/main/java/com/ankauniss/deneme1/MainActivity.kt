1182a87a14500dc96e82d05216e16e312f6745b3
sedanursavas <svs.seda60@gmail.com>
2022-08-08 16:22:12 +0300

    Initial commit

---
+++ app/src/main/java/com/ankauniss/deneme1/MainActivity.kt
@@ -0,0 +1,149 @@
+package com.ankauniss.deneme1
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.UiMode
+import androidx.compose.ui.unit.dp
+import com.ankauniss.deneme1.ui.theme.Deneme1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme{
+                Surface(modifier = Modifier.fillMaxSize()) {
+                   // MessageCard(Message("Eren Jaeger", "Selam Seda ben Eren, biliyorsun bugün 8 Ağustos pazartesi. Jetpack Compose çalışmaya devam et ve lütfen daha hızlı ilerlemeye çalış. Kolay gelsin :)"))
+                    Conversation(SampleData.conversationSample)
+                }
+            }
+
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable()
+fun MessageCard(msg: Message) {
+    //uiMode = Configuration.UI_MODE_NIGHT_YES
+
+    // Add padding around our message
+    Row(modifier = Modifier.padding(8.dp)) {
+        Image(painter = painterResource(id = R.drawable.eren),
+            contentDescription = "Anime Character: Eren",
+            Modifier
+                .size(40.dp)
+                .clip(CircleShape) // Set image size to 40 dp, Clip image to be shaped as a circle
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2)
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier.animateContentSize().padding(1.dp)
+                ) {
+                // Add a vertical space between the author and message texts
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = msg.body,
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1
+                    )
+                /*
+                // surfaceColor will be updated gradually from one color to the other
+                val surfaceColor by animateColorAsState(
+                    if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+                )
+                 */
+            }
+
+        }
+
+    }
+
+}
+
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+/*
+Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+*/
+
+/*
+//Dark Mode bu şekilde yazdım fakat bende çalışmadı
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+*/
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MaterialTheme{
+        Surface {
+            Conversation(SampleData.conversationSample)
+        }
+    }
+}
